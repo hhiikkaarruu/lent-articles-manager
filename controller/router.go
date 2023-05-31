9075be064be085edaedd13c4310f44c90689dd7d
hhiikkaarruu <hhiikkaarruu828@gmail.com>
2023-05-31 12:06:13 +0900

    divide controller

--- controller/router.go
+++ controller/router.go
@@ -1,17 +1,11 @@
 package controller
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/gin"
 )
 
 func GetRouter() (r *gin.Engine) {
 	r = gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
-		})
-	})
+	r.GET("/ping", showJsonPong)
 	return
 }
